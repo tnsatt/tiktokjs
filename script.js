5df18facc7d46796a3e830dc80cd10a731efe407
tnsatt <44207684+tnsatt@users.noreply.github.com>
2022-04-22 13:36:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -12,18 +12,46 @@
 // @exclude      */signup*
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-    document.addEventListener("DOMContentLoaded", function(event) {
-        run_script();
-    });
+    console.log("content")
+    let isext=true;
+    if (window.darkmode_on === undefined) window.darkmode_on = localStorage.getItem("darkmode_on") !== "0";
+    else {
+        isext=true;
+        localStorage.setItem("darkmode_on", window.darkmode_on?1:0)
+    }
+    if (window.multicol_on === undefined) window.multicol_on = localStorage.getItem("multicol_on") !== "0";
+    else localStorage.setItem("multicol_on", window.multicol_on?1:0)
 
-    function run_script(){
-            addstyle(`
+    if(document.head){
+        run_script();
+    }else{
+        // document.addEventListener("DOMContentLoaded", function (event) {
+        //     run_script();
+        // });
+        waitHead(function(){
+            run_script();
+        })
+    }
+    function waitHead(callback){
+        wait();
+        function wait(){
+            if(document.head){
+                callback();
+                return;
+            }
+            setTimeout(function(){
+                wait();
+            }, 100);
+        }
+    }
+    function run_script() {
+        let multicol_style = (`
         [class*="DivBodyContainer"]{min-width:100% !important;}
         [class*="DivScrollContainer-StyledScroll"], [class*="DivSideNavContainer"]{max-width:220px;}
-        [class*="DivMainContainer"]{margin-bottom:40px;max-width:none !important;width:fit-content !important; flex:0 1 auto !important;}
-        [class*="DivMainContainer"] >div{display:flex !important;flex-wrap:wrap !important;justify-content:space-between;}
+        [class*="DivMainContainer"]{margin-bottom:40px;max-width:none !important;width:fit-content !important; flex:1 1 auto !important;}
+        [class*="DivMainContainer"] >div{display:flex !important;flex-wrap:wrap !important;justify-content:space-around;}
         [class*="DivMainContainer"] [data-e2e="recommend-list-item-container"]{width:fit-content !important;max-width:none !important;display:flex !important;flex-direction: row-reverse;margin-left:4px;margin-right:4px;position:relative;}
 
 
@@ -36,10 +64,14 @@
         [class*="DivMainContainer"] [data-e2e="video-author-avatar"]{margin-top:5px;margin-right:5px;}
         [class*="DivMainContainer"] [class*="FollowButton"]{top:80px !important;}
 
+        [data-e2e="recommend-list-item-container"]:has([data-e2e="recommend-video-text"]){flex: 0 1 100% !important;}
+        [data-e2e="recommend-video-text"]{margin-left: unset !important;}
+        [data-e2e="search-user-container"]{flex: 1 0 50%;}
+        [class*="DivMoreContainer"]{flex: 1 0 100%;}
         #darkmode_checkbox{position:fixed; top:0; right:0;}
         [class*="ButtonGetAppText"]{display:none;}
         `);
-         let dmstyle=(`
+        let darkmode_style = (`
          body{--dm-bg:#161722; --main-bg:#060716; --pop-bg:#060716, --menu-bg:#060716; --dm-bg-container:#3d4b5f; --btn-bg: #252632; --input-bg: #252632;
          --span-bg:black; --hover-bg:#060716;}
          *{color:white !important;}
@@ -56,7 +88,7 @@
 
 
          [class*="DivHeaderContainer"], [class*="DivScrollContainer"], .header-content, .header-container, [class*="DivTabsContainer"],
-         .conversation-container, .conversation-list-wrapper, #main .left-part, #main .right-part
+         .conversation-container, .conversation-list-wrapper, #main .left-part, #main .right-part, body > .container
          {background-color:var(--main-bg) !important;}
 
          .menu-right .popup, .modal, .inbox-wrap-enter-done, .message-setting-container, [class*="DivProfileContainer"] [class*="DivContainer"], [class*="DivActionItemContainer"] [class*="DivContainer"],
@@ -66,17 +98,17 @@
          [class*="FormReport"], [class*="FormReport"] [class*="DivFooter"], [class*="DivModalContainer"] [class*="DivContentContainer"]>[class*="Container"],
          [class*="DivKeyboardShortcutContainer"], [class*="tiktok-web-header"], [class*="tiktok-web-body-modal"],[class*="footer-bottom-wrapper"],
          [class*="DivItemBackground"],  [class*="DivContainer"], [class*="DivCashierPage"], [class*="DivCashierPage"] div, [class*="FooterContainer"], [class*="keyboard-shortcut-container"],
-         [class*="-modal__modal-wrapper"], [class*="DivFormBox"]
+         [class*="-modal__modal-wrapper"], [class*="DivFormBox"], [class*="DivModalContainer"] [class*="DivWapper"]
          {background-color:var(--main-bg) !important;}
 
          [class*="DivProfileContainer"] [data-e2e="user-card-follow"],
          [class*="DivCoinContainer"] button, .message-setting-container button,
-         div:not([data-e2e="live-room-follow"]):not([class*="DivFollowButtonWrapper"]) > [class*="FollowButton"],
-         [type="button"]
+         div:not([data-e2e="live-room-follow"]):not([class*="DivFollowButtonWrapper"]) > [class*="FollowButton"]
          {background:var(--btn-bg)}
 
          .area-list-container, form[class*="download-wrapper"], .is-helpful-container button, [data-e2e="nav-login-button"], [class*="SendCode"], [class*="login-button"],
-         .tiktok-select-selector, .tiktok-select-dropdown, .tiktok-select-dropdown-item, [class*="StyledPayButton"]
+         .tiktok-select-selector, .tiktok-select-dropdown, .tiktok-select-dropdown-item, [class*="StyledPayButton"], [class*="ButtonGetAppText"],
+         [class*="ButtonC"], .open_anyway_btn
          {background:var(--btn-bg) !important}
 
          [class*="StyledMessageButton"]{background:var(--main-bg) !important;}
@@ -88,11 +120,16 @@
          {background-color: var(--input-bg);}
 
          input, textarea, [class*="DivOptionsWarpper"], [class*="switch__switch-wrapper"], .tiktok-switch__switch-wrapper, [class*="OptionsContainer"],
-         [class*="InputNum"]
+         [class*="InputNum"], .message-container .text-container, .emojiSelectPopoverGroupList
          {background-color:var(--input-bg) !important;}
 
+         .editor-container .comment-input-inner-wrapper, .editor-container [contenteditable="true"], .editor-container span{
+             background-color: white!important; color: black !important;
+         }
+
          .conversation-list-item-wrapper, .change-video-btn, .preview-v2, [class*="IActionButton"], [class*="download-row-block"], [class*="download-row-block"] button:disabled,
-         button:disabled
+         button:disabled, [data-e2e="user-page"] [class*="DivContainer"], [data-e2e="browse-user-avatar"] [class*="DivContainer"],
+         [data-e2e="search-user-avatar"] [class*="DivContainer"]
          {background-color:transparent !important}
 
          [class*="InputNum"]
@@ -120,66 +157,59 @@
          [class*="DivMainContainer"] [class*="StyledFollowButton"]{}
          html ::-webkit-scrollbar-thumb, html *::-webkit-scrollbar-thumb{background-color: white !important;}
          `)
-          setupdarkmode();
-         window.addEventListener("scroll", function(){
-             document.documentElement.dispatchEvent(new CustomEvent('scroll'));
-
-         })
-            function setupdarkmode(){
-                let val=localStorage.getItem("darkmode")=="1";
-                document.head.insertAdjacentHTML("beforeend", "<style "+(val?"":"type='text'")+" id='darkmode'>"+dmstyle+"</style>");
-                let elm= document.createElement("input");
-                elm.type="checkbox";
-                elm.id="darkmode_checkbox"
-                elm.style.zIndex="99999";
-                elm.checked=val;
-                document.body.appendChild(elm);
-                 elm.addEventListener("change", function(e){
-                     e.stopPropagation();
-                     e.preventDefault();
-                     let val=localStorage.getItem("darkmode")!="1";
-                     setdarkmode(val);
-                     localStorage.setItem("darkmode", val?1:0);
-                 });
-            }
-          function setdarkmode(val){
-              let elm=document.querySelector("style#darkmode")
-              if(val){
-                  elm.removeAttribute("type");
-              }else{
-                  elm.setAttribute("type", "text");
-              }
-          }
-    }
-
-    function addstyle(style){
-        document.head.insertAdjacentHTML("beforeend", "<style>"+style+"</style>");
-    }
-    function waitBody(callback){
-        var observer = new MutationObserver(function() {
-            if (document.body) {
-                callback();
-                observer.disconnect();
-            }
-        });
-        observer.observe(document.documentElement, {childList: true});
-    }
-     function wait_dom(selector, callback, root=document.documentElement){
-         if (document.querySelector(selector)) {
-            return callback(document.querySelector(selector));
+        setup_darkmode();
+        setup_multicol();
+       
+        function setup_multicol(){
+            window.addEventListener("scroll", function () {
+                document.documentElement.dispatchEvent(new CustomEvent('scroll'));
+            })
+            let val = window.multicol_on;
+            let elm=document.getElementById("multicol");
+            if(elm) elm.remove();
+            document.head.insertAdjacentHTML("beforeend", "<style " + (val ? "" : "type='text'") + " id='multicol'>" + multicol_style + "</style>");
         }
-
-        const observer = new MutationObserver(mutations => {
-            let node=document.querySelector(selector);
-            if (node) {
-                callback(node);
-                observer.disconnect();
+        function setup_darkmode() {
+            let val = window.darkmode_on;
+            let elm=document.getElementById("darkmode");
+            if(elm) elm.remove();
+            document.head.insertAdjacentHTML("beforeend", "<style " + (val ? "" : "type='text'") + " id='darkmode'>" + darkmode_style + "</style>");
+            if(!isext){
+                let elm = document.createElement("input");
+                elm.type = "checkbox";
+                elm.id = "darkmode_checkbox"
+                elm.style.zIndex = "99999";
+                elm.checked = val;
+                document.head.appendChild(elm);
+                elm.addEventListener("change", function (e) {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    let val = localStorage.getItem("darkmode") != "1";
+                    set_darkmode(val);
+                });
             }
-        });
+        }
+        window.set_darkmode = function(val) {
+            let elm = document.querySelector("style#darkmode")
+            if (val) {
+                elm.removeAttribute("type");
+            } else {
+                elm.setAttribute("type", "text");
+            }
+            localStorage.setItem("darkmode_on", val ? 1 : 0);
+        }
+        window.set_multicol = function(val) {
+            let elm = document.querySelector("style#multicol")
+            if (val) {
+                elm.removeAttribute("type");
+            } else {
+                elm.setAttribute("type", "text");
+            }
+            localStorage.setItem("multicol_on", val ? 1 : 0);
+        }
+    }
 
-        observer.observe(root, {
-            childList: true,
-            subtree: true
-        });
+    function addstyle(style) {
+        document.head.insertAdjacentHTML("beforeend", "<style>" + style + "</style>");
     }
 })();
